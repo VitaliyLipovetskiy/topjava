e02fae7b40cea5f7091c709c7583310928199916
Vitaliy Lipovetskiy <vitaliy1972li@gmail.com>
2022-06-19 14:31:50 +0300

    Test realise with fix SpringMain, InMemory*Test

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -14,9 +14,9 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 import static ru.javawebinar.topjava.UserTestData.NOT_FOUND;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-@ContextConfiguration("classpath:spring/spring-app.xml")
+@ContextConfiguration("classpath:spring/spring-app-test.xml")
 @RunWith(SpringRunner.class)
-@Ignore
+//@Ignore
 public class InMemoryAdminRestControllerSpringTest {
 
     @Autowired
