4ae2d6469cb6e942afe34db31f54fc6f03874826
Vitaliy Lipovetskiy <vitaliy1972li@gmail.com>
2022-06-23 12:06:18 +0300

    refactoring

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -14,7 +14,6 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,7 +37,7 @@ public class MealServiceTest {
     @Test
     public void get() {
         Meal meal = service.get(100003, USER_ID);
-        assertThat(meal).isEqualTo(meal_100003);
+        assertThat(meal).as("check %s", meal.getDescription()).isEqualTo(meal_100004);
     }
 
     @Test(expected = NotFoundException.class)
@@ -53,7 +52,8 @@ public class MealServiceTest {
         service.delete(100005, USER_ID);
         service.delete(100006, USER_ID);
         service.delete(100007, USER_ID);
-        assertThat(service.getAll(USER_ID)).isEqualTo(Arrays.asList(meal_100008, meal_100009));
+        assertThat(service.getAll(USER_ID)).containsOnly(meal_100008, meal_100009);
+//                .isEqualTo(Arrays.asList(meal_100008, meal_100009));
     }
 
     @Test(expected = NotFoundException.class)
@@ -65,13 +65,15 @@ public class MealServiceTest {
     public void getBetweenInclusive() {
         List<Meal> meals = service.getBetweenInclusive(LocalDate.of(2020, Month.JANUARY, 30),
                 LocalDate.of(2020, Month.JANUARY, 30), USER_ID);
-        assertThat(meals).isEqualTo(Arrays.asList(meal_100003, meal_100004, meal_100005));
+        assertThat(meals).containsOnly(meal_100003, meal_100004, meal_100005);
+        //.isEqualTo(Arrays.asList(meal_100003, meal_100004, meal_100005));
     }
 
     @Test
     public void getAll() {
         List<Meal> all = service.getAll(USER_ID);
-        assertThat(all).isEqualTo(Arrays.asList(meal_100006, meal_100007, meal_100008, meal_100009, meal_100003, meal_100004, meal_100005));
+        assertThat(all).containsOnly(meal_100006, meal_100007, meal_100008, meal_100009, meal_100003, meal_100004, meal_100005);
+//                .isEqualTo(Arrays.asList(meal_100006, meal_100007, meal_100008, meal_100009, meal_100003, meal_100004, meal_100005));
     }
 
     @Test
@@ -83,16 +85,26 @@ public class MealServiceTest {
         assertThat(service.get(100003, USER_ID)).isEqualTo(updated);
     }
 
+    @Test(expected = NotFoundException.class)
+    public void updateNotFound() throws Exception {
+        Meal updated = new Meal(meal_100003);
+        updated.setCalories(6000);
+        updated.setDescription("Updated");
+        service.update(updated, USER_ID);
+        assertThat(service.get(100003, GUEST_ID)).isEqualTo(updated);
+    }
+
     @Test
     public void create() {
         Meal newMeal = new Meal(LocalDateTime.now(), "testing name", 200);
         Meal created = service.create(newMeal, GUEST_ID);
         newMeal.setId(created.getId());
-        assertThat(service.getAll(GUEST_ID)).isEqualTo(Arrays.asList(newMeal));
+        assertThat(service.getAll(GUEST_ID)).containsOnly(newMeal);
+//                .isEqualTo(Arrays.asList(newMeal));
     }
 
     @Test(expected = DataAccessException.class)
-    public void duplicateUserAndDateTimeCreate() throws Exception {
+    public void duplicateDateTimeCreate() throws Exception {
         service.create(new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500), USER_ID);
     }
 }
